75f7e292bbda80182e9a24fb7a001a2287c9d96d
krzysztofwitkowski <kriskw1999@gmail.com>
2024-06-11 13:55:02 +0200

    feat(small improvements): refactor coord

--- src/game.rs
+++ src/game.rs
@@ -1,5 +1,7 @@
 use crate::coord::Coord;
 
+const SPEED: u8 = 8;
+
 #[derive(PartialEq)]
 pub enum Direction {
     Up,
@@ -24,11 +26,7 @@ pub struct Game {
     pub direction: Direction,
     pub point_coord: Option<Coord>,
     pub frame_num: i32,
-    speed: i32,
-}
-
-fn get_direction(position: f64, target: f64) -> f64 {
-    if position < target { 1.0 } else if position > target { -1.0 } else { 0.0 }
+    speed: u8,
 }
 
 impl Game {
@@ -42,7 +40,7 @@ impl Game {
             direction: Direction::Right,
             point_coord: None,
             frame_num: 0,
-            speed: 8,
+            speed: SPEED,
         }
     }
 
@@ -53,24 +51,16 @@ impl Game {
 
     pub fn increase_frame_num(&mut self) {
         self.frame_num += {
-            if self.frame_num > 10 / self.speed { -self.frame_num } else { 1 }
+            if self.frame_num > 10 / (self.speed as i32) { -self.frame_num } else { 1 }
         };
     }
 
     pub fn move_head(&mut self) {
         match self.direction {
-            Direction::Up => {
-                self.head_coord.y += 1.0;
-            }
-            Direction::Down => {
-                self.head_coord.y -= 1.0;
-            }
-            Direction::Left => {
-                self.head_coord.x -= 1.0;
-            }
-            Direction::Right => {
-                self.head_coord.x += 1.0;
-            }
+            Direction::Up => self.head_coord.move_up(1.0),
+            Direction::Down => self.head_coord.move_down(1.0),
+            Direction::Left => self.head_coord.move_left(1.0),
+            Direction::Right => self.head_coord.move_right(1.0),
         }
     }
 
@@ -82,27 +72,17 @@ impl Game {
         let tail_coord = &self.corners[0];
 
         if self.corners.len() == 1 {
-            let x_diff = get_direction(tail_coord.x, self.head_coord.x);
-            let y_diff = get_direction(tail_coord.y, self.head_coord.y);
+            let tail_distance_x = Coord::get_x_distance(tail_coord, &self.head_coord);
+            let tail_distance_y = Coord::get_y_distance(tail_coord, &self.head_coord);
 
-            if
-                (tail_coord.x - self.head_coord.x).abs() > (self.total_length as f64) ||
-                (tail_coord.y - self.head_coord.y).abs() > (self.total_length as f64)
-            {
-                self.corners[0] = Coord::new(tail_coord.x + x_diff, tail_coord.y + y_diff);
+            if tail_distance_x + tail_distance_y > (self.total_length as f64) {
+                self.corners[0].move_toward(&self.head_coord);
             }
         } else {
-            let last_corner_coord = &self.corners[1];
+            let next_corner = self.corners[1].clone();
+            self.corners[0].move_toward(&next_corner);
 
-            let x_diff = get_direction(tail_coord.x, last_corner_coord.x);
-            let y_diff = get_direction(tail_coord.y, last_corner_coord.y);
-
-            if
-                tail_coord.x + x_diff != last_corner_coord.x ||
-                tail_coord.y + y_diff != last_corner_coord.y
-            {
-                self.corners[0] = Coord::new(tail_coord.x + x_diff, tail_coord.y + y_diff);
-            } else {
+            if self.corners[0].compare(&next_corner) {
                 self.corners.remove(0);
             }
         }
@@ -125,12 +105,7 @@ impl Game {
     }
 
     pub fn generate_new_point(&mut self, width: f64, height: f64) {
-        self.point_coord = Some(
-            Coord::new(
-                (rand::random::<f64>() * width - 1.0 - width / 2.0).round(),
-                (rand::random::<f64>() * height - 1.0 - height / 2.0).round()
-            )
-        );
+        self.point_coord = Some(Coord::randomize(width, height));
     }
 
     pub fn check_beat(&mut self) {
